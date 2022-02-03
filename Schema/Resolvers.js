0a8880ff266fd7423d7cf5f71e4984891cd771e8
mananmehta22 <manan.mehta@sts.in>
2022-02-03 22:22:35 +0530

    json

--- Schema/Resolvers.js
+++ Schema/Resolvers.js
@@ -5,17 +5,23 @@ const resolvers = {
     getUpcoming() {
       fetch(`https://api.spacexdata.com/v5/launches/upcoming`)
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => console.log(data.length))
+
+    
     },
     getPast() {
       fetch(`https://api.spacexdata.com/v5/launches/past`)
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => {
+            var past = JSON.stringify(data.length);
+            console.log(typeof(past));
+            return past;
+        })
     },
     getAll() {
       fetch(`https://api.spacexdata.com/v5/launches`)
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => console.log(data.length));
     },
   },
 };
