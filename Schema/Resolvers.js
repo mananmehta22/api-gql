d534480c8963c06ac72ff1e731a24a4d2ba6ab2d
mananmehta22 <manan.mehta@sts.in>
2022-02-07 17:28:05 +0530

    schema

--- Schema/Resolvers.js
+++ Schema/Resolvers.js
@@ -3,25 +3,41 @@ import fetch from "node-fetch";
 const resolvers = {
   Query: {
     getUpcoming() {
-      fetch(`https://api.spacexdata.com/v5/launches/upcoming`)
-        .then((response) => response.json())
-        .then((data) => console.log(data.length))
-
-    
+      fetch(`https://api.spacexdata.com/v3/launches/upcoming`)
+        .then((response) => 
+        response.json())
+        .then((data) => {
+          var upcoming = JSON.stringify(data.length);
+          console.log(upcoming);
+          return upcoming;
+        })
+        .catch(err => {
+          console.log(err)
+        })
     },
-    getPast() {
-      fetch(`https://api.spacexdata.com/v5/launches/past`)
+    getPast(root ,args) {
+      fetch(`https://api.spacexdata.com/v3/launches/past`)
         .then((response) => response.json())
         .then((data) => {
-            var past = JSON.stringify(data.length);
-            console.log(typeof(past));
-            return past;
+          var past = JSON.stringify(data.length);
+          console.log(past);
+          return past;
+        })
+        .catch(err => {
+          console.log(err)
         })
     },
     getAll() {
-      fetch(`https://api.spacexdata.com/v5/launches`)
+      fetch(`https://api.spacexdata.com/v3/launches`)
         .then((response) => response.json())
-        .then((data) => console.log(data.length));
+        .then((data) => {
+          var all = JSON.stringify(data.length);
+          console.log(all);
+          return all;
+        })
+        .catch(err => {
+          console.log(err)
+        })
     },
   },
 };
--- Schema/TypeDefs.js
+++ Schema/TypeDefs.js
@@ -3,44 +3,11 @@ import { gql } from "apollo-server-express";
 const typeDefs = gql`
   # Queries
   type Query {
-    getUpcoming: [Int]
-    getPast: Past
-    getAll: All
+    getUpcoming: String!
+    getPast: String!
+    getAll: String!
   }
 
-  type Upcoming {
-    upcoming: Int
-  }
 
-  type Past{
-    past: String
-  }
-
-  type All {
-    fairings: String
-    static_fire_date_utc: String
-    static_fire_date_unix: String
-    net: Boolean
-    window: String
-    rocket: String
-    success: String
-    details: String
-    launchpad: String
-    flight_number: Int
-    name: String
-    date_utc: String
-    date_unix: Int
-    date_local: String
-    date_precision: String
-    upcoming: Boolean
-    auto_update: Boolean
-    tbd: Boolean
-    launch_library_id: String
-    id: String
-    payloads: [String]
-    capsules: [String]
-    ships: [String]
-    failures: [String]
-  }
 `;
 export default typeDefs;
