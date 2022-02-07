60c474472b8ed0138daeb473e6d8fd5e9aaf8fbb
mananmehta22 <manan.mehta@sts.in>
2022-02-07 23:25:53 +0530

    fixed schema and resolver

--- Schema/Resolvers.js
+++ Schema/Resolvers.js
@@ -2,42 +2,41 @@ import fetch from "node-fetch";
 
 const resolvers = {
   Query: {
-    getUpcoming() {
-      fetch(`https://api.spacexdata.com/v3/launches/upcoming`)
-        .then((response) => 
-        response.json())
+    async getUpcoming() {
+      return fetch(`https://api.spacexdata.com/v3/launches/upcoming`)
+        .then((response) => response.json())
         .then((data) => {
           var upcoming = JSON.stringify(data.length);
           console.log(upcoming);
           return upcoming;
         })
-        .catch(err => {
-          console.log(err)
-        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    getPast() {
-      fetch(`https://api.spacexdata.com/v3/launches/past`)
+    async getPast() {
+      return fetch(`https://api.spacexdata.com/v3/launches/past`)
         .then((response) => response.json())
         .then((data) => {
           var past = JSON.stringify(data.length);
           console.log(past);
           return past;
         })
-        .catch(err => {
-          console.log(err)
-        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     getAll() {
-      fetch(`https://api.spacexdata.com/v3/launches`)
+      return fetch(`https://api.spacexdata.com/v3/launches`)
         .then((response) => response.json())
         .then((data) => {
           var all = JSON.stringify(data.length);
           console.log(all);
           return all;
         })
-        .catch(err => {
-          console.log(err)
-        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
 };
