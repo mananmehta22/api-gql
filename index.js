36e1b7ef9aa6130e189e53722837f326ec7dd4dd
Manan <manan.mehta@sts.in>
2022-02-16 22:43:08 +0530

    cors fix

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from "express";
 import apolloServer from "apollo-server-express";
 import typeDefs from "./Schema/TypeDefs.js";
 import resolvers from "./Schema/Resolvers.js";
+import cors from 'cors';
 const { ApolloServer } = apolloServer;
+
 
 async function startApolloServer(typeDefs, resolvers){
     const server = new ApolloServer({ typeDefs, resolvers })
-    const app = express()
+    const app = express();
+    app.use(cors({ origin: "*"}));
     await server.start();
     server.applyMiddleware({ app })
     
